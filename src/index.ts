6097a6677023a05f661b1168698983e0b66411d5
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-05-31 12:40:34 +0300

    initialize the app and install dependencies and connect DB

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import express from "express";
+
+const server = express();
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
