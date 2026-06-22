1c84635afcf41472595599258eba7d276b4fdd39
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-06-22 21:37:13 +0300

    fix bugs

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 // rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window`
+  max: 500, // Limit each IP to 100 requests per `window`
   message: "Too many requests from this IP, please try again after 15 minutes",
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
@@ -33,7 +33,7 @@ app.use(
 app.use(
   clerkMiddleware({
     authorizedParties: ["http://localhost:5173"],
-    clockSkewInMs: 60000, // tolerate 60 seconds of clock drift
+    clockSkewInMs: 120000, // tolerate 60 seconds of clock drift
   }),
 );
 
